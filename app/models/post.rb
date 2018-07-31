c1ac3a3097b9525dddf9768009a99ad82201c45b
Kriton Dolias <kriton.dolias@nytimes.com>
2018-07-31 01:02:30 +0000

    class method added

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,16 +2,25 @@ class Post < ActiveRecord::Base
       validates :title, :body, presence: true
       validates :title, uniqueness:true
       
-      belongs_to :user
+      #belongs_to :user
+      ## is not needed right now, but will be later - would be used to search over the user that made teh changes.
+      
+      def self.really_long_posts
+          Post.where("length(body) > 10").count
+      
+      end
       
       def length_of_post
          "#{body.split(" ").length} words"
-          
       end
       
      def length_of_title
          "#{title.split(" ").length} words"
-          
      end
+     
+     def body_char
+           self.body.length
+     end
+     
       
 end
